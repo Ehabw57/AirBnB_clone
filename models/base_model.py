02c7c0004a94cbdb1e66d6e35c512672d9ca05d2
Ehabw57 <maestro.ehab123@gmail.com>
2024-01-08 18:09:14 +0200

    Implemented BaseModle class

--- models/base_model.py
+++ models/base_model.py
@@ -3,21 +3,30 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel():
-    def __init__(self):
+    """some docment goes here"""
+    def __init__(self, *args, **kwargs):
         """this is some doc test"""
-        self.id = str(uuid.uuid4)
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            self.updated_at = datetime.fromisoformat(self.updated_at)
+            self.created_at = datetime.fromisoformat(self.created_at)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
     def __str__(self):
         """soem docstring goes here"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-    
+
     def save(self):
-         """some docs string goes here"""
-         self.updated_at = datetime.now()
-    
+        """some docs string goes here"""
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         """somedoctstring goes here"""
         new_dict = self.__dict__.copy()
@@ -25,5 +34,3 @@ class BaseModel():
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
-        
-    
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""somedocmoents goes  here"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """some docs goes here"""
+    def setUp(self):
+        """some doc goes here"""
+        self.b1 = BaseModel()
+        self.b2 = BaseModel()
+
+    def tearDown(self):
+        """some doc goeshere"""
+        del self.b1
+        del self.b2
+
+    def test_uid(self):
+        """some doc here"""
+        self.assertTrue(isinstance(self.b1.id, str), "Uid should be a string")
+        self.assertFalse(self.b1.id == self.b2.id, "Uid should'nt be the same")
+
+    def test_time(self):
+        """seom mother fu docs"""
+        self.assertNotEqual(self.b1.created_at, self.b2.created_at,
+                            "instance can't have same created time")
+        self.assertNotEqual(self.b1.updated_at, self.b2.updated_at,
+                            "instances can't have the same updated time")
+
+    def test_str_method(self):
+        """some doc string goes here"""
+        expected = f"[{self.b1.__class__.__name__}]
+        ({self.b1.id}) {self.b1.__dict__}"
+        self.assertEqual(str(self.b1), expected,
+                         "The output should be the same")
+
+    def test_save_method(self):
+        """some mf doc goes here"""
+        self.assertFalse(self.b1.updated_at > self.b2.updated_at,
+                         "first instance can't be newer than sex instance")
+        self.b1.save()
+        self.assertTrue(self.b1.updated_at > self.b2.updated_at,
+                        "Save should updated the time")
+
+    def test_dict_method(self):
+        """soem doc string goes here"""
+        dict = self.b1.to_dict()
+        self.assertEqual(dict['__class__'], self.b1.__class__.__name__,
+                         "Both items shoud be the values")
+        self.assertEqual(dict['created_at'], self.b1.created_at.isoformat(),
+                         "Both should be the same value")
+        self.assertEqual(dict['updated_at'], self.b1.updated_at.isoformat(),
+                         "Both should be the same value")
+
+    def test_init(self):
+        """soem docs goes here"""
+        json_dict = self.b1.to_dict()
+        self.b0 = BaseModel(**json_dict)
+
+        for key in json_dict.keys():
+            if key not in ['updated_at', 'created_at', '__class__']:
+                self.assertEqual(json_dict[key], self.b0.__dict__[key],
+                                 f"[{key}] this what we are testing now")
+
+        self.assertEqual(type(self.b0.updated_at), type(self.b1.updated_at),
+                         "Updated+created must be instances of datetime class")
+        self.assertEqual(type(self.b0.created_at), type(self.b1.created_at),
+                         "Updated+created must be instances of datetime class")
+        self.assertFalse('__class__' in self.b0.__dict__)
+
+
+if __name__ == "___main__":
+    unittest.main()
