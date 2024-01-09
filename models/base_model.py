0b51c6b3c06e5a6f7fb1e9f61dca7bdd24ce4d79
Ehabw57 <maestro.ehab123@gmail.com>
2024-01-09 20:37:07 +0200

    Fixed a bug in the engine file

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime
 from models import storage
 
-
 class BaseModel():
     """some docment goes here"""
     def __init__(self, *args, **kwargs):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,6 @@
 """soem fukn docstriong goes here"""
 import json
 
-
 class FileStorage():
     """some freaking doc goes here"""
     __file_path = 'data.json'
@@ -15,15 +14,20 @@ class FileStorage():
     def new(self, obj):
         """doc doc doc"""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj
         
     def save(self):
         """doki doki"""
+        json_dict = FileStorage.__objects.copy()
+        for key in json_dict.keys():
+            json_dict[key] = json_dict[key].to_dict()
+
         with open(FileStorage.__file_path, 'w') as data_json:
-           json.dump(FileStorage.__objects, data_json)
+           json.dump(json_dict, data_json)
 
     def reload(self):
         """doco doco"""
+        from models.base_model import BaseModel
         try:
             with open(FileStorage.__file_path, 'r') as data_json:
                 FileStorage.__objects = json.load(data_json)
@@ -31,4 +35,7 @@ class FileStorage():
             pass
         except Exception:
             pass
+        for key in FileStorage.__objects.keys():
+            FileStorage.__objects[key] = BaseModel(**FileStorage.__objects[key])
+
         
